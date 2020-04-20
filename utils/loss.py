c5a49edc461762ade43216985d12c15f14025115
BikangPeng <peng920324@gmail.com>
2020-04-20 22:05:17 +0800

    update

---
+++ utils/loss.py
@@ -0,0 +1,207 @@
+#!usr/bin/python
+# -*- encoding: utf-8 -*-
+"""
+@Time           : 2020/4/20 下午9:55
+@User           : kang
+@Author         : BiKang Peng
+@ProjectName    : Lane_Segmentation_torch
+@FileName       : loss.py 
+@Software       : PyCharm   
+"""
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+from scipy.ndimage import distance_transform_edt
+
+
+def sum_tensor(inp, axes, keepdim=False):
+    if keepdim:
+        for ax in axes:
+            inp = inp.sum(int(ax), keepdim=True)
+    else:
+        for ax in sorted(axes, reverse=True):
+            inp = inp.sum(int(ax))
+    return inp
+
+
+def softmax_helper(x):
+    rpt = [1 for _ in range(len(x.size()))]
+    rpt[1] = x.size(1)
+    x_max = x.max(1, keepdim=True)[0].repeat(*rpt)
+    e_x = torch.exp(x - x_max)
+    return e_x / e_x.sum(1, keepdim=True).repeat(*rpt)
+
+
+def get_tp_fp_fn(net_output, gt, axes=None, mask=None, square=False):
+    """
+    net_output必须是（b，c，x，y（，z））
+    gt必须是标签图（形状（b，1，x，y（，z））或形状（b，x，y（，z） ））或一种热编码（b，c，x，y（，z））
+    如果提供了mask，则它必须具有形状（b，1，x，y（，z）））
+    @param net_output:
+    @param gt:
+    @param axes:
+    @param mask:
+    @param square:
+    @return:
+    """
+
+    if axes is None:
+        axes = tuple(range(2, len(net_output.size())))
+
+    shp_x = net_output.shape
+    shp_y = gt.shape
+
+    with torch.no_grad():
+        if len(shp_x) != len(shp_y):
+            gt = gt.view((shp_y[0], 1, *shp_y[1:]))
+
+        if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
+            # if this is the case then gt is probably already a one hot encoding
+            y_onehot = gt
+        else:
+            gt = gt.long()
+            y_onehot = torch.zeros(shp_x)
+            if net_output.device.type == "cuda":
+                y_onehot = y_onehot.cuda(net_output.device.index)
+            y_onehot.scatter_(1, gt, 1)
+
+    tp = net_output * y_onehot
+    fp = net_output * (1 - y_onehot)
+    fn = (1 - net_output) * y_onehot
+
+    if mask is not None:
+        tp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(tp, dim=1)), dim=1)
+        fp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fp, dim=1)), dim=1)
+        fn = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fn, dim=1)), dim=1)
+
+    if square:
+        tp = tp ** 2
+        fp = fp ** 2
+        fn = fn ** 2
+
+    tp = sum_tensor(tp, axes, keepdim=False)
+    fp = sum_tensor(fp, axes, keepdim=False)
+    fn = sum_tensor(fn, axes, keepdim=False)
+
+    return tp, fp, fn
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, apply_nonlin=softmax_helper, alpha=0.5, gamma=2, balance_index=0, smooth=1e-5,
+                 size_average=True):
+        super(FocalLoss, self).__init__()
+        self.apply_nonlin = apply_nonlin
+        self.alpha = alpha
+        self.gamma = gamma
+        self.balance_index = balance_index
+        self.smooth = smooth
+        self.size_average = size_average
+
+        if self.smooth is not None:
+            if self.smooth < 0 or self.smooth > 1.0:
+                raise ValueError('smooth value should be in [0,1]')
+
+    def forward(self, logit, target):
+        if self.apply_nonlin is not None:
+            logit = self.apply_nonlin(logit)
+        num_class = logit.shape[1]
+
+        if logit.dim() > 2:
+            # N,C,d1,d2 -> N,C,m (m=d1*d2*...)
+            logit = logit.view(logit.size(0), logit.size(1), -1)
+            logit = logit.permute(0, 2, 1).contiguous()
+            logit = logit.view(-1, logit.size(-1))
+        target = torch.squeeze(target, 1)
+        target = target.view(-1, 1)
+        alpha = self.alpha
+
+        if alpha is None:
+            alpha = torch.ones(num_class, 1)
+        elif isinstance(alpha, (list, np.ndarray)):
+            assert len(alpha) == num_class
+            alpha = torch.FloatTensor(alpha).view(num_class, 1)
+            alpha = alpha / alpha.sum()
+        elif isinstance(alpha, float):
+            alpha = torch.ones(num_class, 1)
+            alpha = alpha * (1 - self.alpha)
+            alpha[self.balance_index] = self.alpha
+
+        else:
+            raise TypeError('Not support alpha type')
+
+        if alpha.device != logit.device:
+            alpha = alpha.to(logit.device)
+
+        idx = target.cpu().long()
+
+        one_hot_key = torch.FloatTensor(target.size(0), num_class).zero_()
+        one_hot_key = one_hot_key.scatter_(1, idx, 1)
+        if one_hot_key.device != logit.device:
+            one_hot_key = one_hot_key.to(logit.device)
+
+        if self.smooth:
+            one_hot_key = torch.clamp(
+                one_hot_key, self.smooth / (num_class - 1), 1.0 - self.smooth)
+        pt = (one_hot_key * logit).sum(1) + self.smooth
+        logpt = pt.log()
+
+        gamma = self.gamma
+
+        alpha = alpha[idx]
+        alpha = torch.squeeze(alpha)
+        loss = -1 * alpha * torch.pow((1 - pt), gamma) * logpt
+
+        if self.size_average:
+            loss = loss.mean()
+        else:
+            loss = loss.sum()
+        return loss
+
+    class TverskyLoss(nn.Module):
+        def __init__(self, apply_nonlin=softmax_helper, batch_dice=True, do_bg=True, smooth=1.,
+                     square=False):
+            super(TverskyLoss, self).__init__()
+
+            self.square = square
+            self.do_bg = do_bg
+            self.batch_dice = batch_dice
+            self.apply_nonlin = apply_nonlin
+            self.smooth = smooth
+            self.alpha = 0.3
+            self.beta = 0.7
+
+        def forward(self, x, y, loss_mask=None):
+            shp_x = x.shape
+
+            if self.batch_dice:
+                axes = [0] + list(range(2, len(shp_x)))
+            else:
+                axes = list(range(2, len(shp_x)))
+
+            if self.apply_nonlin is not None:
+                x = self.apply_nonlin(x)
+
+            tp, fp, fn = get_tp_fp_fn(x, y, axes, loss_mask, self.square)
+
+            tversky = (tp + self.smooth) / (tp + self.alpha * fp + self.beta * fn + self.smooth)
+
+            if not self.do_bg:
+                if self.batch_dice:
+                    tversky = tversky[1:]
+                else:
+                    tversky = tversky[:, 1:]
+            tversky = tversky.mean()
+
+            return -tversky
+
+    class FocalTversky_loss(nn.Module):
+        def __init__(self, tversky_kwargs, gamma=0.75):
+            super(FocalTversky_loss, self).__init__()
+            self.gamma = gamma
+            self.tversky = TverskyLoss(**tversky_kwargs)
+
+        def forward(self, net_output, target):
+            tversky_loss = 1 + self.tversky(net_output, target)  # = 1-tversky(net_output, target)
+            focal_tversky = torch.pow(tversky_loss, self.gamma)
+            return focal_tversky
